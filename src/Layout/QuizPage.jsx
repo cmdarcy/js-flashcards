0a5d770629d32a82ae9cbe7873cc6a3c6e6dd9a7
Christopher d'Arcy <cmdarcy89@gmail.com>
2024-11-13 23:08:18 -0500

    add under construction quiz page

--- src/Layout/QuizPage.jsx
+++ src/Layout/QuizPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function QuizPage() {
-	return <h2 className="text-white">Quiz</h2>;
+	return (
+		<>
+			<h2 className="text-white text-2xl">Quiz</h2>
+			<p className="text-white">
+				Sorry! This page is currently under construction.
+			</p>
+		</>
+	);
 }
 
 export default QuizPage;
